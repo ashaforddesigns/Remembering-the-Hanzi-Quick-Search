1492e3e05f5aa4797acef3daf067ac4066834014
ashaforddesigns <ashaforddesigns@gmail.com>
2024-12-10 21:13:23 -0600

    working code!

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Remembering the Simplified Hanzi Quick Search (using Python) :mahjong: :cn:
-## Project Goals
+## Project Goals :star:
 - Create a quick search through all 3000 of James Heisig by using the character or the keyword
 - Allow the user to add notes, which could be their own story that they use to remember the hanzi
+
+## Tasks :lightbulb:
 
 ##### Special Thanks
 Thank you [PavelDanov](https://gist.github.com/PavelDanov/176fc3904d384155a041aa8a0778f494) for the list of Heisig's simplified hanzi letters already in .txt format
--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import json
 
-
-
 with open("rsh_flashcards.json","r") as json_file:
     data = json.load(json_file)
 
-
 def hanziSearch(hanzi):
 #spit out keyword and allow to edit quick notes
-    print(f"Searching for {hanzi}...")
+    print(f"Searching for '{hanzi}'...")
     #logic
     for index, details in data.items():
         if details["hanzi"] == hanzi:
@@ -33,7 +30,27 @@ def hanziSearch(hanzi):
 
 def keywordSearch(keyword):
 #spit out hanzi and allow to edit quick notes
-    pass
+    print(f"Searching for '{keyword}'")
+    #logic
+    for index, details in data.items():
+        if details['keyword'] == keyword:
+            print(f"Hanzi: {details['hanzi']}")
+            print(f"Additional notes: {details["notes"]}")
+            task = input("Press [N] to update a Note, [Q] to quit or hit Enter to make a new search ").capitalize()
+            if task == "N":
+                myNote = input("Enter your note now: ")
+                noteAdded = addNote(keyword, myNote)  # addNote will return True if successful
+                if noteAdded: # is True
+                    print(f"Your note '{myNote}' was successfully updated!")
+                else:
+                    print("Something went wrong, try again.")
+                    break
+            if task == "Q":
+                print("Exiting...")
+                break
+            else:
+                NewSearch()
+
     
 def addNote(searchTerm, myNote):
     # Checks if the note is empty
@@ -55,12 +72,12 @@ def addNote(searchTerm, myNote):
             details["notes"] = myNote  
             with open("rsh_flashcards.json", "w") as json_file:
                 json.dump(data, json_file, indent=4)
-            return True
-    return False
+            return True #it was a success
+    return False #it failed (should not happen)
 
 
 def NewSearch():
-    print("Welcome to Asha's Remembering the Hanzi Quick Search!")
+    print("Welcome to Asha's Remembering the Hanzi Quick Search!!!")
     print("Are you searching by [H]anzi or a [K]eyword?")
     searchType = input("Please select [H] or [K] ").capitalize()
 
